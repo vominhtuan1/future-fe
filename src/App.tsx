0deadf1b1ee97ed49a557f74373da756d0a77a13
vominhtuan1 <19522487@gm.uit.edu.vn>
2023-05-13 14:17:13 +0700

    chore: change some style for aboutCard

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,14 @@ import Home from "./pages/home";
 import About from "./pages/about";
 import CartItem from "./components/ui/CarItem/CarItem";
 import WishList from "./components/ui/WishList/WishList";
+import AboutCard from "./components/ui/Strengths/AboutCard";
 function App() {
   return (
-    <div>
-      <WishList></WishList>
+    <div className="px-[75px]">
+      <AboutCard icon="lock" title="Safe Delivery">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry Lorem Ipsum has
+      </AboutCard>
     </div>
   );
 }
