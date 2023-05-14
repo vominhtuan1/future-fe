e40fae28fff5e0a26e1055ac61faf253471eaa12
vominhtuan1 <19522487@gm.uit.edu.vn>
2023-05-14 09:45:33 +0700

    feat: about us page

--- src/components/ui/quote.tsx
+++ src/components/ui/quote.tsx
@@ -18,15 +18,17 @@ export default function Quote({
   address,
 }: Props) {
   return (
-    <div className="space-y-5">
+    <div className="space-y-5 select-none">
       <QuoteAltLeft />
-      <p className="text-heading-7 font-semibold">{title}</p>
-      <p className="text-body-3 font-normal text-black/50 !mt-2">{subTitle}</p>
-      <div className="flex gap-x-4 items-center">
+      <p className="font-semibold text-heading-8">{title}</p>
+      <p className="text-body-3 font-normal text-philippine-gray !mt-2">
+        {subTitle}
+      </p>
+      <div className="flex items-center gap-x-4">
         <Avatar alt={name} src={avatar} sx={{ width: 56, height: 56 }} />
         <div>
-          <p className="text-heading-7 font-semibold">{name}</p>
-          <p>{address}</p>
+          <p className="font-semibold text-heading-8">{name}</p>
+          <p className="text-philippine-gray text-body-3">{address}</p>
         </div>
       </div>
     </div>
